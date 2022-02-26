78ab6e25e165f207b7d8fb25dbd80b8156024b6d
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-02-26 12:46:05 +0100

    conver into upercase

---
+++ week-1/InClass/exercise-1.js
@@ -0,0 +1,11 @@
+// Exercise 1
+function nameInUperCase(string){
+    return string.toUpperCase()
+}
+   console.log( nameInUperCase('hello daniel'));
+   console.log( nameInUperCase('hello irina'));
+   console.log( nameInUperCase('hello mimi'));
+   console.log( nameInUperCase('hello rob'));
+   console.log( nameInUperCase('hello yohannes'));
+
+
